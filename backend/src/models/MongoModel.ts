487cf21722fef9ad806d1b3d22985824143a6f85
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-07 22:09:32 -0300

    feat: criação da class de Model genérica

---
+++ backend/src/models/MongoModel.ts
@@ -0,0 +1,54 @@
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+
+export default abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async getAll(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  // public async getByParams(_id: string): Promise<T[] | null> {
+  //   // if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+  //   // return this._model.findOne({ _id });
+  //   return null;
+  // }
+
+  public async getOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOne({ _id });
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async update(_id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );
+  }
+
+  public async updatePartial(_id: string, obj: Partial<T>): Promise<T | null> {
+    // if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    // return this._model.findByIdAndUpdate(
+    //   { _id },
+    //   { ...obj } as UpdateQuery<T>,
+    //   { new: true },
+    // );
+    return null;
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndDelete({ _id });
+  }
+}
