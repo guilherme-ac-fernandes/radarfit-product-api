a42b49ea8e8a6acd742c14af90550cfd0864c484
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-08 15:25:47 -0300

    feat: incluir cors para comunicação com o frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
+
 import ProductRoute from './routes/ProductRoute';
 import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/produtos', ProductRoute);
