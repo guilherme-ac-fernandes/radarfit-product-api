581229eedfa83cdb8d5937e6f199fa5a9551cdac
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-08 13:14:31 -0300

    refactor: correção da formatação e adiciona referência

--- backend/src/models/ProductModel.ts
+++ backend/src/models/ProductModel.ts
@@ -2,15 +2,18 @@ import { model as mongooseCreateModel, Schema } from 'mongoose';
 import { IProduct } from '../interfaces/IProduct';
 import MongoModel from './MongoModel';
 
+// Utilização do timestamps proveniente da documentação do Mongoose
+// source: https://mongoosejs.com/docs/timestamps.html
 const productMongooseSchema = new Schema<IProduct>(
   {
     produto: String,
     valor: Number,
     descricao: String,
   },
-  { versionKey: false,
+  {
+    versionKey: false,
     timestamps: { createdAt: 'created', updatedAt: 'updated' },
-},
+  },
 );
 
 export default class ProductModel extends MongoModel<IProduct> {
