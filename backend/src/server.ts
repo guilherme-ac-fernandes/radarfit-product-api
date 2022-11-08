4c1a6a499ecd0944c3464438582e3f7fbd22d428
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-08 13:15:25 -0300

    refactor: correção do lint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,17 @@ import 'dotenv/config';
 import app from './app';
 import connectToDatabase from './models/connection';
 
+const PORT = process.env.PORT || 3001;
+
 const apiRunning = () => {
   app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
 };
 
 const errorsCatch = (error: Error) => {
   console.log('Connection with database generated an error:\r\n');
-    console.error(error);
-    console.log('\r\nServer initialization cancelled');
-    process.exit(0);
+  console.error(error);
+  console.log('\r\nServer initialization cancelled');
+  process.exit(0);
 };
 
-const PORT = process.env.PORT || 3001;
 connectToDatabase().then(apiRunning).catch(errorsCatch);
