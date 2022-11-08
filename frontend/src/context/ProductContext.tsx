8d8773b72daab65c523bff68fee33710736188b8
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-08 15:47:17 -0300

    feat: adiciona referência para tipagem do contexto

--- frontend/src/context/ProductContext.tsx
+++ frontend/src/context/ProductContext.tsx
@@ -1,6 +1,8 @@
 import { createContext } from 'react';
 import { IProduct } from './ProductProvider';
 
+// Tipagem do contexto proveniente do vídeo presente no Youtube (autor: Huriel)
+// source: https://www.youtube.com/watch?v=fPR-Z56BDyg
 interface IContextValue {
   products: IProduct[],
   loading: boolean,
