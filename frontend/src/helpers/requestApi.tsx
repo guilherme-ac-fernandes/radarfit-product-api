6de3042dec2d67bf4783e9e80704bde4cce6f7db
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-09 00:03:52 -0300

    feat: criação do arquivo de funções que realizam as requisições ao banco de dados

--- frontend/src/helpers/requestApi.tsx
+++ frontend/src/helpers/requestApi.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import IProduct from '../interfaces/IProduct';
 
 const API_URL = 'http://localhost:3001/produtos';
 
@@ -7,3 +8,22 @@ export const getAllProducts = async () => {
   return data;
 };
 
+export const createProduct = async (payload: Partial<IProduct>) => {
+  const { data } = await axios.post(API_URL, payload);
+  return data;
+};
+
+export const updateProduct = async (id: string, payload: Partial<IProduct>) => {
+  const { data } = await axios.put(`${API_URL}/${id}`, payload);
+  return data;
+};
+
+export const deleteProduct = async (id: string) => {
+  const { data } = await axios.delete(`${API_URL}/${id}`);
+  return data;
+};
+
+export const getProductsBySearch = async (q: string) => {
+  const { data } = await axios.get(`${API_URL}/search?q=${q}`);
+  return data;
+};
