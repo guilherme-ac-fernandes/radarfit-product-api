788495e2c6ba8183a97d847bba7e9928e4fd148a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-09 00:07:59 -0300

    feat: construção da página com a combinação dos componentes criados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,83 @@
-import Header from "./components/Header";
-import DisplayProducts from "./components/DisplayProducts";
-import ProductProvider from "./context/ProductProvider";
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import DisplayProducts from './components/DisplayProducts';
+import DetailsProduct from './components/DetailsProduct';
+import IProduct from './interfaces/IProduct';
+import axios from 'axios';
+import AddFormProduct from './components/AddFormProduct';
+
+const API_URL = 'http://localhost:3001/produtos';
 
 export default function App() {
+  const [addProduct, setAddProduct] = useState(false);
+  const [detailsProduct, setDetailsProduct] = useState<IProduct | {}>({});
+  const [products, setProducts] = useState<IProduct[] | []>([]);
+  const [favoriteProducts, setFavoriteProducts] = useState<IProduct[] | []>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [search, setSearch] = useState<string>('');
+
+  useEffect(() => {
+    const storageFavorites: string | null = localStorage.getItem("favoriteProducts");
+    if (storageFavorites !== null) {
+      const parsedFavorites = JSON.parse(storageFavorites);
+      setFavoriteProducts(parsedFavorites);
+    }
+
+    const getAllProducts = async () => {
+      setLoading(true);
+      const { data } = await axios.get(API_URL);
+      setProducts(data);
+      setLoading(false);
+    };
+    getAllProducts();
+  }, []);
+
+  useEffect(() => {
+    if (search.length === 0) {
+      const getAllProducts = async () => {
+        setLoading(true);
+        const { data } = await axios.get(API_URL);
+        setProducts(data);
+        setLoading(false);
+      };
+      getAllProducts();
+    }
+    const getProductsBySearch = async () => {
+      setLoading(true);
+      const { data } = await axios.get(`${API_URL}/search?q=${search}`);
+      setProducts(data);
+      setLoading(false);
+    };
+    getProductsBySearch();
+  }, [search])
+
   return (
-    <ProductProvider>
-      <Header />
-      <DisplayProducts />
-    </ProductProvider>
+    <main>
+      <Header search={search} setSearch={setSearch} />
+      <h1>Produtos</h1>
+      <button disabled={addProduct} onClick={() => setAddProduct(true)}>
+        +
+      </button>
+      {addProduct && (
+        <AddFormProduct handleClose={() => setAddProduct(false)} />
+      )}
+
+      <section>
+        <DisplayProducts
+          products={products}
+          loading={loading}
+          favoriteProducts={favoriteProducts}
+          setDetailsProduct={setDetailsProduct}
+        />
+        {'produto' in detailsProduct && (
+          <DetailsProduct
+            product={detailsProduct}
+            favoriteProducts={favoriteProducts}
+            setFavoriteProducts={setFavoriteProducts}
+            setProducts={setProducts}
+          />
+        )}
+      </section>
+    </main>
   );
 }
