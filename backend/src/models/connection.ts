03a9256310c3ba6be29a69675f1056d59a524f28
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-10 06:56:28 -0300

    feat: atualização da url do mongo para uma presente no railway

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/RadarFit';
+const {
+  MONGOHOST,
+  MONGOPASSWORD,
+  MONGOPORT,
+  MONGOUSER,
+} = process.env;
+
+const MONGO_DB_URL = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`;
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
